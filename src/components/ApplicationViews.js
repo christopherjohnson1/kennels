4db0b280ce0439bae1a53a334b8ab3aa0bf597b7
Chris Johnson <christopherjohnson1@gmail.com>
2020-09-08 11:42:56 -0500

    auth works

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,20 +1,20 @@
+  
 import React from "react"
 import { Route } from "react-router-dom"
 import { LocationProvider } from "./location/LocationProvider"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { LocationList } from "./location/LocationList"
 import { AnimalList } from "./animal/AnimalList"
-import { CustomerProvider } from "./customer/CustomerProvider"
-import { CustomerList } from "./customer/CustomerList"
-import { EmployeeProvider } from "./employee/EmployeeProvider"
-import { EmployeeList } from "./employee/EmployeeList"
-import { EmployeeForm } from "./employee/EmployeeForm"
+import { CustomerProvider } from "./customer/CustomerProvider.js"
+import { CustomerList } from "./customer/CustomerList.js"
+import { EmployeeProvider } from "./employee/EmployeeProvider";
+import { EmployeeList } from "./employee/EmployeeList";
+import { EmployeeForm } from "./employee/EmployeeForm.js"
 
 export const ApplicationViews = (props) => {
     return (
         <>
             <LocationProvider>
-                {/* Render the location list when http://localhost:3000/ */}
                 <Route exact path="/">
                     <LocationList />
                 </Route>
@@ -23,10 +23,10 @@ export const ApplicationViews = (props) => {
             <AnimalProvider>
                 <CustomerProvider>
                     <LocationProvider>
-                {/* Render the animal list when http://localhost:3000/animals */}
-                <Route path="/animals">
-                    <AnimalList />
-                </Route>
+                        <Route path="/animals" render={(props) => {
+                            return <AnimalList history={props.history} />
+                        }} />
+
                     </LocationProvider>
                 </CustomerProvider>
             </AnimalProvider>
@@ -37,17 +37,28 @@ export const ApplicationViews = (props) => {
                 </Route>
             </CustomerProvider>
 
+            <Route path="/logout" render={
+                (props) => {
+                    localStorage.removeItem("kennel_customer")
+                    props.history.push("/login")
+                }
+            } />
+
+            <EmployeeProvider>
+                <AnimalProvider>
+                    <LocationProvider>
+                        <Route path="/employees/create" render={(props) => {
+                            return <EmployeeForm {...props} />
+                        }} />
+                    </LocationProvider>
+                </AnimalProvider>
+            </EmployeeProvider>
+
             <EmployeeProvider>
                 <LocationProvider>
-                    <AnimalProvider>
-                        <Route exact path="/employees" render={
-                            props => <EmployeeList {...props} />
-                        } />
-                        <Route exact path="/employees/create" render={
-                            props => <EmployeeForm {...props} />
-                        }>
-                        </Route>
-                    </AnimalProvider>
+                    <Route exact path="/employees" render={(props) => {
+                        return <EmployeeList history={props.history} />
+                    }} />
                 </LocationProvider>
             </EmployeeProvider>
         </>
--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,32 +1,34 @@
+
 import React, { useContext, useEffect } from "react"
-import { EmployeeContext } from "./EmployeeProvider"
-import { LocationContext } from "../location/LocationProvider"
-import { Employee } from "./Employee"
+import { EmployeeContext } from "./EmployeeProvider";
+import { LocationContext } from "../location/LocationProvider.js";
 import "./Employee.css"
 
-export const EmployeeList = (props) => {
-    const {employees, getEmployees} = useContext(EmployeeContext)
+export const EmployeeList = (burrito) => {
+    const { employees, getEmployees } = useContext(EmployeeContext)
     const { locations, getLocations } = useContext(LocationContext)
 
     useEffect(() => {
-        getEmployees()
-        getLocations()
+        getEmployees().then(getLocations)
     }, [])
 
     return (
-        <div className="employees">
-            <h1>Employees</h1>
-            <button onClick={() => props.history.push("/employees/create")}>
+        <>
+            <button onClick={() => burrito.history.push("/employees/create")}>
                 Add Employee
             </button>
-        <article className="employeeList">
-            {
-                employees.map(employee => {
-                    const location = locations.find(l => l.id === employee.locationId) || {}
-                    return <Employee key={employee.id} employee={employee} location={location} />
-                })
-            }
-        </article>
-        </div>
+            <article className="employees">
+                {
+                    employees.map(employee => {
+                        const employeeLocation = locations.find(loc => loc.id === employee.locationId) || {}
+                        return <section key={employee.id} className="employee">
+                            <div><h3>{employee.name}</h3></div>
+                            <div>{employeeLocation.name}</div>
+                        </section>
+                    })
+                }
+            </article>
+        </>
     )
+
 }
